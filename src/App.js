1605ba6f9e58b97263349f3065d709ac3d95f1a6
Kelsey Guffanti <kc.breen3@gmail.com>
2023-08-14 22:02:37 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./variables.css";
+import "./App.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  // dark mode toggle:
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <button onClick={toggleTheme} className={`App-toggle-${theme}`}>
+        toggle mode
+      </button>
+
+      <div className="Toggle-mode">
+        <input type="checkbox" className="checkbox" id="chk" />
+        <label className="label" htmlFor="chk">
+          <FontAwesomeIcon icon={faMoon} className="Moon" />
+          <FontAwesomeIcon icon={faSun} className="Sun" />
+          <i className="fas fa-sun"></i>
+          <div className="ball"></div>
+        </label>
+      </div>
+
+      <h1>Hello, world!</h1>
     </div>
   );
 }
